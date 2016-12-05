444cac2e37f513e2c02247c0c104f0fe5f5bd0a5
dje <duanje@126.com>
2016-12-05 18:49:35 +0800

    add ssm project

---
+++ ssm/src/main/java/ssm/controller/UserController.java
@@ -0,0 +1,8 @@
+package ssm.controller;
+
+/**
+ * Created by dje on 2016/12/5.
+ */
+public class UserController {
+
+}
---
+++ ssm/src/main/java/ssm/model/User.java
@@ -0,0 +1,73 @@
+package ssm.model;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Short age;
+
+    private String address;
+
+    private String phone;
+
+    private String username;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
